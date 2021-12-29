c49c15d554104326bd7d0a77a676059fe849b213
John Padilla <johnpadi8790@gmail.com>
2021-12-29 00:13:28 -0700

    Day 3
    UIActivityViewController

---
+++ Project3/Project1/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by John Padilla on 12/22/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var picture = [String]()
+    
+    var selectedPictureNumber = 1
+    var totalPictures = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let item = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in item {
+            if item.hasPrefix("nssl") {
+                picture.append(item)
+                picture.sort()
+            }
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return picture.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = picture[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = picture[indexPath.row]
+            vc.selectedPictureNumber = indexPath.row + 1
+            vc.totalPictures = picture.count
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
+
